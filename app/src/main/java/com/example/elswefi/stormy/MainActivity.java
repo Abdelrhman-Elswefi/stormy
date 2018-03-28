7a1ea07c1dc479ea03405bdc6fc1f22f3d6e3793
Abdelrahman El-swefi <elswefy.ae@gmail.com>
2018-03-28 07:54:09 +0200

    building a get data request from an api using OkHttp library and logging the response.

--- app/src/main/java/com/example/elswefi/stormy/MainActivity.java
+++ app/src/main/java/com/example/elswefi/stormy/MainActivity.java
@@ -14,10 +14,28 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
 
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String apiKey = "5bf205067672bf5fdf7a5b2b416895c0";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forecastUrl = "https://api.darksky.net/forecast/"
+                +apiKey+"/"+latitude+","+longitude;
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+        Call call = client.newCall(request);
+        try {
+            Response response =call.execute();
+        } catch (IOException e) {
+            Log.e(TAG, "Exception caught :", e);
+        }
+
 
     }
 }
