45ab0cbc46e43a10d5c24e1e8d26a0b134e37f48
Abdelrahman El-swefi <elswefy.ae@gmail.com>
2018-03-28 07:39:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/elswefi/stormy/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.elswefi.stormy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
